684bf57f8b417358fa9edc91efef602b55d62edc
Vlad Buhman <vortland@rambler.ru>
2017-03-06 23:37:08 +0300

    SON start

---
+++ Scale_of_notation/Scale_of_notation/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scale_of_notation
+{
+    public class SON
+    {
+        private int[] number;
+        private int scale;
+        public SON(int scale)
+        {
+            this.scale = scale;
+            number = new int[1];
+        }
+        public int this[int index]
+        {
+            get
+            { return number[index]; }
+            set
+            { number[index] = value; }
+        }
+    }
+}
+}
