49952ceeabb021bf52ecbe541fcc5f0a93fc68e2
Vlad Buhman <vortland@rambler.ru>
2017-03-07 11:05:22 +0300

    done SON

--- Scale_of_notation/Scale_of_notation/Class1.cs
+++ Scale_of_notation/Scale_of_notation/Class1.cs
@@ -16,11 +16,23 @@ namespace Scale_of_notation
         }
         public int this[int index]
         {
-            get
-            { return number[index]; }
-            set
-            { number[index] = value; }
+            get { return number[index]; }
+            set { number[index] = value; }
+        }
+        public static SON operator +(SON obj1,int obj2)
+        {
+            for (int i=0;obj2>0;i++)
+            {
+                obj1.number[i] = obj1.number[i] + obj2;
+                if(obj1.number[i]>obj1.scale-1)
+                {
+                    obj2 = obj1.number[i] - obj1.scale;
+                    obj1.number[i] = 0;
+                }
+                Array.Resize(ref obj1.number, obj1.number.Length);
+            }
+            return obj1;
         }
     }
 }
-}
+
